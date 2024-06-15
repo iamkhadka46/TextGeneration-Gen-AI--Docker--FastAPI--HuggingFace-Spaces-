78d41a836843b9e4984fc61f72c725f24d753116
iamkhadka46 <iam.khadka46@gmail.com>
2024-06-15 13:32:47 +0200

    Final Commit

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ from transformers import pipeline
 
 app = FastAPI()
 
-pipe = pipeline("text2text-generation", model="google/flan-t5-base")
+pipe = pipeline("text2text-generation", model="google/flan-t5-small")
 
 
 @app.get("/")
-async def home():
+def home():
     return {"message": "Hello World"}
 
 @app.get("/generate")
-async def generate(text: str):
+def generate(text: str):
     output = pipe(text)
 
     return {"output": output[0]['generated_text']}
